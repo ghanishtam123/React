6b10be2641cabeaa5bce7757b7a0cb63e663a61c
Ghanishta <143160355+ghanishtam123@users.noreply.github.com>
2025-12-15 11:22:52 +0530

    Time: 56 ms (38.54%) | Memory: 58.2 MB (5.20%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+ var createCounter = function(init) {
+    let current = init;   
+
+    return {
+        increment: function() {
+            return ++current;   // increase then return
+        },
+        decrement: function() {
+            return --current;   // decrease then return
+        },
+        reset: function() {
+            current = init;     // reset to initial value
+            return current;
+        }
+    };
+};
+
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
